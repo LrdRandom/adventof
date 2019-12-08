f54c741f62d73351bc3f858a3ec8fd62b84fc24a
Random <lrdrandom@gmail.com>
2019-12-08 09:46:22 +0000

    Initial commit

---
+++ src/main/java/com/saidinit/random/adventof/day1/Day1.java
@@ -0,0 +1,9 @@
+package com.saidinit.random.adventof.day1;
+
+public class Day1 {
+/**
+ * At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. 
+ * They haven't determined the amount of fuel required yet.
+ * */
+	
+}
